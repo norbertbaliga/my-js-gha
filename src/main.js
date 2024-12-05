3f3078b130fccccf009a975ecfe46ac4b3dd9954
Norbert Baliga <82504330+norbertbaliga@users.noreply.github.com>
2024-12-05 16:40:24 +0100

    feat: first version (#2)
    * Update CODEOWNERS
    
    * Update README
    
    * Fix name of the repo in README
    
    * Do renaming everywhere.
    
    * Implement Nexus IQ CLI tool installation
    
    * Add Workflow for testing action
    
    * fix action version tag
    
    * Build bundle
    
    * Move setup logic into main.
    
    * Fix tool name
    
    * Remove unecessary files.
    
    * Delete workflows not needed
    
    * Add .node-version file
    
    * npm bundle

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-const core = require('@actions/core')
-const { wait } = require('./wait')
+const core = require("@actions/core");
+const tc = require("@actions/tool-cache");
 
 /**
  * The main function for the action.
@@ -7,24 +7,53 @@ const { wait } = require('./wait')
  */
 async function run() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
+    // Download URL for the Nexus IQ CLI
+    const cliDownloadURLTenplate =
+      "https://download.sonatype.com/clm/scanner/nexus-iq-cli-{iq-cli-version}.jar";
+    // Get version of tool to be installed
+    const version = core.getInput("version");
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    // Throw an error if the version is not provided
+    if (!version) {
+      throw new Error("Input parameter version is required.");
+    }
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // Replace the version placeholder in the download URL
+    const cliDownloadURL = cliDownloadURLTenplate.replace(
+      "{iq-cli-version}",
+      version,
+    );
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    // File chache path for the tool
+    let cachedToolPath = tc.find("iq-cli", version);
+
+    // If the tool is not already cached, download it
+    if (!cachedToolPath) {
+      core.debug(`Downloading IQ CLI version ${version}`);
+      const toolPath = await tc.downloadTool(
+        cliDownloadURL,
+        "sonatype-iq-cli.jar",
+      );
+      core.debug(`Downloaded IQ CLI to ${toolPath}`);
+      cachedToolPath = await tc.cacheFile(
+        toolPath,
+        "sonatype-iq-cli.jar",
+        "iq-cli",
+        version,
+      );
+    } else {
+      core.debug(`IQ CLI version ${version} found in cache.`);
+    }
+
+    core.setOutput("iq-cli-version", version);
   } catch (error) {
     // Fail the workflow run if an error occurs
-    core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    }
   }
 }
 
 module.exports = {
-  run
-}
+  run,
+};
